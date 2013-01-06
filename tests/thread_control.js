94f516249ac0304a2888efd2be37de43e5cff27a
Ondross <andrew.heine@students.olin.edu>
2013-01-06 16:37:05 -0500

    first commit

--- tests/thread_control.js
+++ tests/thread_control.js
@@ -1,41 +1,15 @@
-window.columns = 1;
-window.currentcolumn = 0;
-window.threadID = 0;
-window.parentThread = 0;
-window.myID = 0;
+function find_recent_substring(caret, string, substring){
+    /* Takes a caret location, string, and substring, and returns
+    the last instance of that substring before the caret location.
+    Returns -1 if the substring does not exist before the caret location
+    */
+  
+  string = string.substring(0, caret + 1);        //cut out string
 
-$(document).ready(function(){
-	$("#thread0").focus();
-	$('textarea').autoResize();
-})
+  var pos = string.indexOf(substring);            //find first instance
+  while(string.indexOf(substring, pos+1) != -1) { //find instances until you run out
+    pos = string.indexOf(substring, pos+1);
+  }
 
-$("#page").keydown(function(e) {
-	var code = (e.keyCode ? e.keyCode : e.which);
-    if(code == 66) {
-    	if (e.ctrlKey){
-    	  //var pos = $("#thread" + myID).atCaret('getCaretPosition');
-    	  //var row = Math.floor((pos - 1)/80.0);
-    	  var caret_height = $("#thread"+ myID).getCaretPosition().top;
-
-    	  parentThread = myID;
-    	  threadID += 1;
-    	  myID += threadID;
-    	  $("#page").append("<textarea class='thread' id='thread" + threadID + "' style='border:none; width:644; outline:none; resize: none; position:absolute; left:" + 660 * (columns) + "; top:" + (caret_height - 6) + ";' placeholder='New Interuption' type='text' wrap='hard' ></textarea>");
-    	  $("#thread" + threadID).focus();
-    	  $("#thread" + threadID).autoResize();
-
-    	}
-    }
-});
-
-$("#page").keydown(function(e) {
-	var code = (e.keyCode ? e.keyCode : e.which);
-    if(code == 73) {
-    	if (e.ctrlKey){
-    	  $("#thread" + parentThread).focus();
-    	  myID = parentThread;
-    	  parentThread -= 1;
-    	}
-    }
-});
-//time to start using js objects. Parent thread, #ints, column, etc.
+  return(pos);
+}

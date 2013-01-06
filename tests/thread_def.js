e578bf6435a45a55c5e6216d3cab2f4c4c3aa0cf
Ondross <andrew.heine@students.olin.edu>
2013-01-06 15:26:11 -0500

    carets move around at will. nothing is function oh god.

--- tests/thread_def.js
+++ tests/thread_def.js
@@ -1,5 +1,5 @@
 function Thread(parent, id, height){
-	this.id = id;
+	this.id = "thread"+id;
 	if (parent==null){parent=this; this.column=-1;}
 	this.parent = parent;
 	this.column = parent.column + 1;

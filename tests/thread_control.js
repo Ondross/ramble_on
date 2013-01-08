6425ced5ca7cfe4b5f9b22223bb2fb650c8578ef
Ondross <andrew.heine@students.olin.edu>
2013-01-08 16:35:15 -0500

    cleaned up code

--- tests/thread_control.js
+++ tests/thread_control.js
@@ -4,12 +4,57 @@ function find_recent_substring(caret, string, substring){
     Returns -1 if the substring does not exist before the caret location
     */
   
-  string = string.substring(0, caret + 1);        //cut out string
+    string = string.substring(0, caret);        //cut out string
 
-  var pos = string.indexOf(substring);            //find first instance
-  while(string.indexOf(substring, pos+1) != -1) { //find instances until you run out
-    pos = string.indexOf(substring, pos+1);
-  }
+    var pos = string.indexOf(substring);            //find first instance
+    while(string.indexOf(substring, pos+1) != -1) { //find instances until you run out
+        pos = string.indexOf(substring, pos+1);
+    }
 
-  return(pos);
+    return(pos);
+}
+
+function get_length_into_tag(caret, string){
+    var last_closer = find_recent_substring(caret, string, ">");
+    if (last_closer == -1){last_closer = -2;}
+    length_into_tag = caret - last_closer;
+    return length_into_tag;
+}
+
+function new_text_area(type, id, placeholder){
+
+    $("#page").append("<textarea class='" + type + "' id='" + id + "' placeholder='" + placeholder + "' type='text' wrap='hard' ></textarea>");
+}
+
+function new_child_thread(parent, height_in_parent){
+    /*creates a new interruption*/
+
+    //create the instance
+    newThread = new Thread(parent, threads, parent.top + height_in_parent);
+    threads += 1;
+    parent.children.push(newThread);
+
+    //write the html
+    new_text_area("thread", newThread.id, "New Interruption");
+    $("#" + newThread.id).focus().css({left: 660 * newThread.column, top: 5 + newThread.top});
+    $("#" + newThread.id).autoResize();
+
+    return newThread;
+}
+
+function change_thread(thread){
+    $("#" + thread.id).focus();
+    return thread;
+}
+
+function move_caret(area_id, location){
+    /*sets the text cursor to location in the specified area
+    */
+    $("#" + area_id).atCaret('setCaretPosition', location);
+}
+
+function get_caret_location(area_id){
+    /* return location of caret
+    */
+    return $("#" + area_id).atCaret('getCaretPosition');
 }
--- tests/thread_def.js
+++ tests/thread_def.js
@@ -1,8 +1,8 @@
-function Thread(parent, id, height){
+function Thread(parent, id, top){
 	this.id = "thread"+id;
 	if (parent==null){parent=this; this.column=-1;}
 	this.parent = parent;
 	this.column = parent.column + 1;
 	this.children = [];
-	this.height = height;
+	this.top = top;
 }
